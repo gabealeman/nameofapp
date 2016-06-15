889803cf401546cc8246aa8088fdef285d728866
Gabriel <ga9065@gmail.com>
2016-06-15 13:07:08 -0500

    fixed mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "aleman.gabril01@gmail.com"
 
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
-        :to => 'your-email@example.com',
+        :to => 'aleman.gabriel01@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
 end
