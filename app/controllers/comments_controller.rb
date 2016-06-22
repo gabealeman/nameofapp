f18c7a9c49d38689407df78fb0fa74b5c086eee6
Gabriel <ga9065@gmail.com>
2016-06-22 17:16:30 -0500

    fixed formatting and errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
 
 	def create
-      @product = Product.find(params[:product_id])
-      @comment = @product.comments.new(comment_params)
-      @comment.user = current_user
-      respond_to do |format|
-      	if @comment.save
-          format.html { redirect_to @product, notice: 'Review was created successfully.' }
-          format.json { render :show, status: :created, location: @product }
-        else 
-        	format.html { redirect_to @product, alert: 'Review was not saved successfully.'}
-        	format.json { render json: @comment.erros, status: unprocessable_entity }
-        end
+  	@product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+    	if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else 
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.'}
+        format.json { render json: @comment.errors, status: unprocessable_entity }
       end
     end
+  end
 
-	def destroy
+	def destroy if signed_in? && current_user.admin?
     @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
     if params[:q]
        search_term = params[:q]
        
-       @products = Product.where("name LIKE ?", "%#{search_term}%")
+       @products = Product.where("name ilike ?", "%#{search_term}%")
     else
       @products = Product.all
     end
@@ -16,8 +16,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    #@comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
-    @comments = Comment.paginate(:page => params[:page], :per_page => 2)
+    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /products/new
