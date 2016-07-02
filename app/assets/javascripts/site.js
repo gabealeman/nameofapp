abe60affcab59efdac7ce7b187beec8142c94b60
Gabriel <ga9065@gmail.com>
2016-07-02 14:33:18 -0500

    functional tests

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,15 @@
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' 
-});
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' 
+  });
+
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+    }
+  });
+
+  $(".mCustomScrollbar").mCustomScrollbar({axis:"x"
+	});
+	
 });
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controler do
+
+	before do
+		@user = User.create!(email: "peter@example.com", password: "1234567890")
+		@user_two = User.create!(email: "jason@example.com", password: "1234567890")
+	end
+
+	describe "GET #show" do
+		context "User is logged in"
+			before do
+				sign_in @user
+			end
+
+			it "loads correct user details" do
+				get :show, id: @user.id
+				expect(response).to have_http_status(200)
+				expect(assigns(:user)).to eq @user
+			end
+		end
+
+		context "No user is logged in" do
+			it "redirects to login" do
+				get :show, id: @user.id
+				expect(response).to redirect_to(root_path)
+			end
+		end
+
+		context "User is logged in" do
+			before do
+				sign_in @user_two
+			end
+
+			context "Wrong user" do
+				it "redirects to login" do
+					get :show, id: @user.id
+					expect(assigns(:user)).not_to eq @user_two
+					expect(response).to redirect_to(root_path)
+				end
+	    end
+	  end
+	end
