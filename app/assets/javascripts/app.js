975a2de8a6c19f1af42577e996a5e9c19cc332f6
Gabriel <ga9065@gmail.com>
2016-07-12 20:14:08 -0500

    fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@ var app = angular.module('shop', ['ngResource']);
 $(document).on('ready page:load', function() {
 	angular.bootstrap(document.body, ['shop'])
 });
-app.factory('models', ['$resource' function($resource){
+app.factory('models', ['$resource', function($resource){
 	var orders_model = $resource("/orders/:id.json", {id: "@id"});
 	var products_model = $resource("/products/:id.json", {id: "@id"});
 	var x = {
@@ -19,8 +19,8 @@ app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 	$scope.products = models.products.query();
 
 	$scope.addOrder = function(){
-		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''{return;
-		})
+		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){return;
+		}
 		$order = models.orders.save($scope.newOrder, function(){
 			recent_order = models.orders.get({id: order.id});
 			$scope.orders.push(recent_order);
