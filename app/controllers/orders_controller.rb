51fc19a7d32ebdac591769ae0577936579365b78
Gabriel <ga9065@gmail.com>
2016-08-05 12:33:41 -0500

    systax fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,15 @@
 class OrdersController < ApplicationController
-	protect_from_forgery prepend: true.
-	#skip_before_action :verify_authenticity_token, if: :json_request?
+	#skip_before_filter  :verify_authenticity_token MAKES it vulnurable for attacks
+	protect_from_forgery with: :null_session
 	respond_to :json, :html
-	load_and_authorize_resource
-	
+
 	def index
 		@orders = Order.all.to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
 		respond_with @orders
 	end
 
 	def show
-		@orders = Order.find(params[:id]).to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
+		@order = Order.find(params[:id]).to_json(:include => [{:product => {:only => :name}}, {:user => {:only => :email}}])
 		respond_with @order
 	end
 
@@ -22,22 +21,18 @@ class OrdersController < ApplicationController
 		respond_with @order
 	end
 
+	# def update
+	# 	respond_with Order.update(params[:id], params[:order])
+	# end
+
 	def destroy
 		respond_with Order.destroy(params[:id])
 	end
 
-	protected
+	private
 
-	def json_request?
-		request.format.json?
-	end
+  def order_params
+    params.require(:order).permit(:product_id, :user_id, :total)
+  end
 
-	def set_order
-		@order = Order.find(params[:id])
-	end
-
-	def order_params
-		params.require(:order).permit(:product_id, :user_id, :total)
-	end
-		
 end
